a816554a6a279b68890976ba8160273b8cdad328
5t4l1n <nexulean@nexulean.info>
2025-07-27 00:36:00 +0530

    panel & coding

--- a.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-db = MongoClient("mongodb://localhost:27017").openlearnx
-exam = db.exams.find_one({"_id": ObjectId("6884f04c6ca73cc9032deaf9")})
-print(exam["exam_code"])      # this is what participants must type
